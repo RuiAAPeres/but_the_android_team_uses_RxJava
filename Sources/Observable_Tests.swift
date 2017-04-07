a906fa986e609c565b72422adacbab91c4c18284
ruiaaperes <rui.peres@babylonhealth.com>
2017-04-07 12:27:52 +0100

    Map + FlatMap tests

--- Sources/Observable_Tests.swift
+++ Sources/Observable_Tests.swift
@@ -61,4 +61,47 @@ final class Observable_Tests: XCTestCase {
         observable.subscribe(action)
         waitForExpectations(timeout: 1, handler: nil)
     }
+
+    func test_map() {
+        let expect = expectation(description: "")
+        let observable = Observable<Int>.just(value: 1)
+
+        var count = 0
+        let action: (but_the_android_team_uses_RxJava.Event<String>) -> Void = { event in
+            switch count {
+            case 0: XCTAssertEqual(event.to_ras_event().value, "1")
+            case 1: expect.fulfill()
+            default: fatalError("Not Expected")
+            }
+            count += 1
+        }
+
+        observable
+            .map(String.init)
+            .subscribe(action)
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
+    func test_flatMap() {
+        let expect = expectation(description: "")
+        let observable = Observable<Int>.just(value: 1)
+
+        var count = 0
+        let action: (but_the_android_team_uses_RxJava.Event<String>) -> Void = { event in
+            switch count {
+            case 0: XCTAssertEqual(event.to_ras_event().value, "1")
+            case 1: expect.fulfill()
+            default: fatalError("Not Expected")
+            }
+            count += 1
+        }
+
+        observable
+            .flatMap { Observable.just(value: String($0)) }
+            .subscribe(action)
+
+        waitForExpectations(timeout: 1, handler: nil)
+    }
+
 }
